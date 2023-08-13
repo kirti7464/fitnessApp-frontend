749d0cdd4da2e9f00fcac79469e79af7f3544e87
kirti7464 <71482336+kirti7464@users.noreply.github.com>
2023-08-13 22:35:40 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,45 +5,45 @@ const NavBar = () => {
   const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.removeItem('token');
-    window.history.replaceState(null, '', '/');
     navigate('/')
   };
   return (
     <div>
-      <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-        <img src="https://www.travthetraveller.com/wp-content/uploads/2020/02/food.png" height={"45px"} alt="img"/>
-        {localStorage.getItem("token") ?( <span className="navbar-brand" >
-            Fitness Recipe
-          </span> ):(
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+  <img src="https://www.travthetraveller.com/wp-content/uploads/2020/02/food.png" height={"35px"} alt="img"/>
+  {localStorage.getItem("token") ?( <>
+    Fitness Recipe
+  </> ):(
             <Link className="navbar-brand" to="/" >
             Fitness Recipe
           </Link>
           )}
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            {localStorage.getItem("token") ? (
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      {localStorage.getItem("token") ? (
                 <li className="nav-item">
                 <Link className="nav-link" to="/recipe">
                   Home
                 </Link>
               </li> 
                 ) : null}  
-             <li className="nav-item">
-                <Link className="nav-link" to="/about">
+        <li className="nav-item">
+        <Link className="nav-link" to="/about">
                   About
                 </Link>
-              </li> 
-                  
-            </ul>
-            {localStorage.getItem("token") ? (
-              <form className="form-inline my-2 my-lg-0">
-                <button className="btn btn-outline-success my-2 my-sm-0" onClick={handleLogout } type="submit">Logout</button>
-              </form>
-                ) : null}
-          </div>
-        </div>
-      </nav>
+        </li>
+     
+      </ul>
+      {localStorage.getItem("token") ? (<form className="d-flex">
+        <button className="btn btn-outline-success" onClick={handleLogout } type="submit">Logout</button>
+      </form>) : null}
+    </div>
+  </div>
+</nav>  
     </div>
   );
 };
